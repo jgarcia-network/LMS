a4555aaccd18a11a32c42c73823f20d478c10283
jgarcia-network <3106189+jgarcia-network@users.noreply.github.com>
2020-07-30 18:43:51 -0700

    initial push

---
+++ application/src/main/java/com/lms/application/entity/LearningPlan.java
@@ -0,0 +1,62 @@
+package com.lms.application.entity;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+public class LearningPlan {
+	
+	private Long id;
+	private LocalDate dateAdded;
+	@JsonIgnoreProperties("user")
+	private User user;
+	private Set<Course> courses;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getDateAdded() {
+		return dateAdded;
+	}
+
+	public void setDateAdded(LocalDate dateAdded) {
+		this.dateAdded = dateAdded;
+	}
+
+	@OneToOne
+	@JoinColumn(name = "userId")
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	@ManyToMany(mappedBy = "plan")
+	public Set<Course> getCourses() {
+		return courses;
+	}
+	
+	public void setCourses(Set<Course> courses) {
+		this.courses = courses;
+	}
+
+}
